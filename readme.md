5c18f6a577c447099e4e95c33cdd7be0683428e4
alexdemichieli <alexdemichieli@github.com>
2024-06-20 22:56:26 -0700

    adding heroku deployment

---
+++ readme.md
@@ -0,0 +1,8 @@
+deploy with heroku:
+
+1) install heroku CLI
+2) run `heroku login`
+3) run `heroku create`
+4) run `git push heroku main`
+5) run `heroku ps:scale web=1`
+
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const productRoutes = require('./routes/API/products');
 require('dotenv').config();
 app.use(express.json());
